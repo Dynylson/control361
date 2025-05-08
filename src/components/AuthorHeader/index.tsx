34af64d8f9e88d977b520a94bd8540efdc7bfdce
Dynylson Júnior <dynylsonjr@gmail.com>
2025-05-07 21:50:01 -0300

    feat: finalizar componente VehicleToolbar

--- src/components/AuthorHeader/index.tsx
+++ src/components/AuthorHeader/index.tsx
@@ -5,7 +5,7 @@ interface AuthorHeaderProps {
 export function AuthorHeader({ fullName }: AuthorHeaderProps) {
     return (
         <header className="flex items-center bg-[#001E2E] h-[60px]">
-            <h1 className="text-white text-[1.125rem] ml-6">{fullName}</h1>
+            <h1 className="text-white text-[1.125rem] font-medium ml-6">{fullName}</h1>
         </header>
     )
 }
--- src/components/VehicleToolbar/index.tsx
+++ src/components/VehicleToolbar/index.tsx
@@ -2,9 +2,22 @@ import { VehicleType } from "./components/VehicleType";
 
 export function VehicleToolbar() {
     return (
-        <section>
-            <h2>Lista</h2>
-            <VehicleType />
+        <section className="flex justify-between items-center">
+            <div className="flex items-center gap-36">
+                <h2 className="text-[1.25rem] font-semibold">Lista</h2>
+                <VehicleType />
+            </div>
+            <div className="flex items-center gap-4">
+                <input
+                type="text" 
+                placeholder="Buscar por placa ou frota" 
+                className="
+                    border-1 border-[#89919B] rounded-md p-2 focus:outline-none 
+                    focus:border-[#0095E4] focus:ring-1 focus:ring-[#0095E4]
+                    w-[280px] h-[40px]"
+                />
+                <button className="bg-[#0095E4] w-[150px] h-[40px] rounded-lg cursor-pointer">Novo</button>
+            </div>
         </section>
     )
 }
