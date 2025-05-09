d03590982c9766dfc8c3c824d08fa91953be4fae
Dynylson Júnior <dynylsonjr@gmail.com>
2025-05-08 21:53:41 -0300

    feat: criar instância do axios

---
+++ src/services/axios.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export const api = axios.create({
+    baseURL: import.meta.env.VITE_BASE_URL_API,
+    headers: {
+      'Authorization': `Bearer ${import.meta.env.VITE_TOKEN_API}`,
+      'Content-Type': 'application/json',
+    },
+});
