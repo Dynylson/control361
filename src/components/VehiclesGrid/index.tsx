76b2e7716e649a1ac1daef6a36c017419684f2c0
Dynylson Júnior <dynylsonjr@gmail.com>
2025-05-11 17:33:14 -0300

    test: garantir que o spinner de loading esteja sendo mostrado enquanto é feito a requisição de veículos para preencher a grid

--- src/components/VehiclesGrid/index.tsx
+++ src/components/VehiclesGrid/index.tsx
@@ -32,7 +32,7 @@ export function VehiclesGrid() {
 
   if (isLoading) return (
     <div className="flex justify-center items-center h-64">
-      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-[#002D44]"></div>
+      <div className="animate-spin spin-vehicles-grid rounded-full h-12 w-12 border-t-2 border-b-2 border-[#002D44]"></div>
     </div>
   );
 
--- src/tests/e2e/components/VehiclesGrid.spec.tsx
+++ src/tests/e2e/components/VehiclesGrid.spec.tsx
@@ -13,3 +13,10 @@ test('should display vehicles in the grid', async ({ page }) => {
   await expect(firstVehicle).toContainText('Modelo');
   await expect(firstVehicle).toContainText('Status');
 });
+
+test('should show loading spinner while fetching vehicles', async ({ page }) => {
+  await page.goto(process.env.VITE_BASE_URL_API || 'http://localhost:5173');
+
+  const loadingSpinner = await page.locator('.spin-vehicles-grid');
+  await expect(loadingSpinner).toBeVisible();
+});
