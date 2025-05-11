d9622dad085eb086134d068603d9d73afeaa9b94
Dynylson Júnior <dynylsonjr@gmail.com>
2025-05-11 01:19:10 -0300

    feat: tornar o componente VehicleToolbar responsivo para mobile

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <VehiclesProvider>
       <main>
         <AuthorHeader fullName="Dynylson Júnior" />
-        <div className="mx-11 mt-7">
+        <div className="mx-4 sm:mx-6 md:mx-11 mt-4 sm:mt-6 md:mt-7">
           <VehicleToolbar />
           <FleetMap />
           <VehiclesGrid />
--- src/components/VehicleToolbar/index.tsx
+++ src/components/VehicleToolbar/index.tsx
@@ -2,27 +2,27 @@ import { VehicleType } from "./components/VehicleType";
 
 export function VehicleToolbar() {
     return (
-        <section className="flex justify-between items-center border-b-2 border-[#002D44] pb-5">
-            <div className="flex items-center gap-36">
-                <h2 className="text-[1.25rem] font-semibold">Lista</h2>
+        <section className="flex flex-col md:flex-row justify-between items-start md:items-center border-b-2 border-[#002D44] pb-5 gap-4 md:gap-0">
+            <div className="flex flex-col sm:flex-row items-start sm:items-center gap-4 sm:gap-36 w-full md:w-auto">
+                <h2 className="text-[1.25rem] font-semibold hidden md:block">Lista</h2>
                 <VehicleType />
             </div>
-            <div className="flex items-center gap-4">
+            <div className="flex flex-col sm:flex-row items-start sm:items-center gap-2 sm:gap-4 w-full md:w-auto">
                 <input
-                type="text" 
-                placeholder="Buscar por placa ou frota" 
-                className="
-                    border-2 border-[#89919B] rounded-lg p-3 focus:outline-none 
-                    focus:border-[#0095E4] focus:ring-[1px] focus:ring-[#0095E4]
-                    w-[280px] h-[40px] text-[#8A939D] placeholder-[#8A939D]"
+                    type="text"
+                    placeholder="Buscar por placa ou frota"
+                    className="
+                        border-2 border-[#89919B] rounded-lg p-3 focus:outline-none 
+                        focus:border-[#0095E4] focus:ring-[1px] focus:ring-[#0095E4]
+                        w-full sm:w-[280px] h-[40px] text-[#8A939D] placeholder-[#8A939D]"
                 />
                 <button className="
-                    bg-[#0095E4] w-[150px] h-[40px] rounded-lg cursor-pointer 
+                    bg-[#0095E4] w-full sm:w-[150px] h-[40px] rounded-lg cursor-pointer 
                     transition duration-200 hover:brightness-110"
                 >
                     Novo
                 </button>
             </div>
         </section>
-    )
+    );
 }
